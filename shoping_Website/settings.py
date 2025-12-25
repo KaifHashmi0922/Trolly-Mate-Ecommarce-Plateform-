50fe16a5d35fd4962a977e036522b3ba28f428f0
KAIF <kmo57787@gmail.com>
2025-12-25 14:03:34 +0530

    Templates Bug Fix

--- shoping_Website/settings.py
+++ shoping_Website/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'shoping_Website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
